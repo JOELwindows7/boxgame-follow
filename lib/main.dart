c4e99867715701ea419db788677910e7c8a07a2d
Joel Robert Justiawan <joelwindows7@gmail.com>
2020-03-09 15:58:18 +0700

    Add main Menu
    also in Android Studio there is Git built in

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:boxgame/Scaffoldings.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
 import 'box-game.dart';
-import 'package:flutter/gestures.dart';
+//import 'package:flutter/gestures.dart';
 
 // https://jap.alekhin.io/create-mobile-game-flutter-flame-beginner-tutorial
 // https://jap.alekhin.io/2d-casual-mobile-game-tutorial-flame-flutter-part-1
@@ -22,8 +23,67 @@ Future<void> main() async {
 //  runApp(game.widget);
 //  flameUtil.addGestureRecognizer(tapper);
 
-  runApp(containsGame());
+  runApp(StartPoint());
 }
+
+class StartPoint extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StartScaffold(),
+    );
+  }
+}
+
+class StartScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to Burning Flame'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => containsScaffold()
+                  ),
+                );
+              },
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.play_arrow),
+                  Text('Play Game'),
+                ],
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                // https://stackoverflow.com/questions/45109557/flutter-how-to-programmatically-exit-the-app
+                SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+              },
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.exit_to_app),
+                  Text('Quit Game'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
 
 class containsScaffold extends StatelessWidget {
   BoxGame game = BoxGame();
@@ -49,7 +109,7 @@ class containsScaffold extends StatelessWidget {
                 RaisedButton(
                   onPressed: (){
                     print('Press Accesibility');
-
+                    Navigator.pop(context);
                   },
                   child: Icon(Icons.accessibility),
                 ),
@@ -68,7 +128,7 @@ class containsScaffold extends StatelessWidget {
         },
         child: Icon(Icons.restaurant_menu),
       ),
-    );;
+    );
   }
 }
 
